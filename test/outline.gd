45e1595155b1f10a63710226ca9f93d1f61d1966
Quanthon <Quanthon@163.com>
2024-08-20 03:26:09 +0800

    12

--- test/outline.gd
+++ test/outline.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _ready():
-	Input.set_custom_mouse_cursor(preload("res://assets/img/expand.svg"))
+	Input.set_custom_mouse_cursor(preload("res://assets/img/expand.svg"), 0, Vector2(11, 11))
 
 
 func _process(delta: float) -> void:
